c317ffc258636f9fef3213a8b3c779387e0a9604
Adir <slutzkis@gmail.com>
2023-05-22 12:20:22 +0300

    updates

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function colorCount(){
-    
+    addColor(Number(document.getElementById("input-slider").value));
+}
+
+function addColor(){
+    document.getElementById("main-flex").innerHTML += '<svg id="container-1" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="70" fill="#f00"/></svg>';
 }
