910533b2d23758f4c6f11a92610f8f266fe7a45d
Adir <slutzkis@gmail.com>
2023-05-22 12:22:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function colorCount(){
+    window.countNum = 0;
+    document.getElementById("main-flex").innerHTML = "";
     addColor(Number(document.getElementById("input-slider").value));
 }
 
-function addColor(){
-    document.getElementById("main-flex").innerHTML += '<svg id="container-1" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="70" fill="#f00"/></svg>';
+function addColor(a){
+    if(a > window.countNum){
+        document.getElementById("main-flex").innerHTML += '<svg id="container-1" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="70" fill="#f00"/></svg>';
+        window.countNum += 1;
+        addColor(Number(document.getElementById("input-slider").value));
+    }
 }
