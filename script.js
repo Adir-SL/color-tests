dd076e0cb2a011ef29ffaa9afb93063be8c6bb1c
Adir <slutzkis@gmail.com>
2023-05-29 11:27:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function colorCount(){
     window.countNum = 0;
     document.getElementById("main-flex").innerHTML = "";
-    addColor(Number(document.getElementById("input-slider").value));
+    addColor(6);
 }
 
 function addColor(a){
     if(a > window.countNum){
         document.getElementById("main-flex").innerHTML += '<svg id="container-1" class="color-container" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="100" cy="100" r="70" fill="#f00"/></svg>';
         window.countNum += 1;
-        addColor(Number(document.getElementById("input-slider").value));
+        addColor(6);
     }
     updateColors(a);
 }
