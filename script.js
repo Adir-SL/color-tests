b289d58e007f4b17c519da8b3c2dbe20fd7a4bd5
Adir <slutzkis@gmail.com>
2023-05-30 10:43:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function colorCount(){
 
 function addColor(a){
     if(a > window.countNum){
-        document.getElementById("main-flex").innerHTML += '<svg id="container-1" class="color-container" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="192" height="192" rx="32" fill="#f00" stroke="#ddd"/></svg>';
+        document.getElementById("main-flex").innerHTML += '<svg id="container-1" class="color-container" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="192" height="192" rx="32" fill="#f00" stroke="#ddd" stroke-width="2"/></svg>';
         window.countNum += 1;
         addColor(6);
     }
